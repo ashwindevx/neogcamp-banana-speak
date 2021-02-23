a7d125e68f349f17b3fe70754f30e85e9c414000
Ashwin Chauhan <ashwinchauhan.developer@gmail.com>
2021-02-23 17:14:50 +0530

    done

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const textArea = document.querySelector('#txt-input');
-const btn = document.querySelector('#btn-translate');
-const output = document.querySelector('#output');
+const btnTranslate = document.querySelector('#btn-translate');
+const txtInput = document.querySelector('#txt-input');
+const outputDiv = document.querySelector('#output');
 
-function translate() {
-    let sentence = textArea.value;
-    output.innerHTML = sentence;
+// let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
 }
-btn.addEventListener('click', translate);
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert('something went wrong!');
+}
+
+function clickHandler() {
+    const inputText = txtInput.value; // taking input
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            let translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
+}
+
+btnTranslate.addEventListener('click', clickHandler);
